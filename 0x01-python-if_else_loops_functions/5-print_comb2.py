612e147cc7b5732823f4370072fd1e855777cc2c
DanielN24 <capdaniel_98@outlook.com>
2022-05-03 18:40:35 -0500

    new file with the answer number 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for zero_to_nine in range(00, 100):
-    if zero_to_nine < 99:
-        print("{0:02d}".format(zero_to_nine), ',', end='')
-    else:
-        print(zero_to_nine)
+for zero_to_nine in range(00, 99):
+    print("{:02d}".format(zero_to_nine), ',', end='')
+print(99)
