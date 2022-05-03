5b4969ded4b47de5bdc165736b6214f8309991d9
DanielN24 <capdaniel_98@outlook.com>
2022-05-03 15:52:36 -0500

    new file with the answer number 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0:
-    lastnumber = number % 10
-    if lastnumber > 5:
-        print("Last digit of", number, "is", lastnumber, "and is greater than 5")
-    elif lastnumber < 6 and lastnumber != 0:
-        print("Last digit of", number, "is", lastnumber, "and is less than 6 and not 0")
+if number >= 0:
+    ln = number % 10
+    if ln > 5:
+        print("Last digit of", number, "is", ln, "and is greater than 5")
+    elif ln < 6 and ln != 0:
+        print("Last digit of", number, "is", ln, "and is less than 6 and not 0")
     else:
-        print("Last digit of", number, "is", lastnumber, "and is 0")
+        print("Last digit of", number, "is", ln, "and is 0")
 
 if number < 0:
-    lastnumber = number % -10
-    if lastnumber > 5:
-        print("Last digit of", number, "is", lastnumber, "and is greater than 5")
-    elif lastnumber < 6 and lastnumber != 0:
-     print("Last digit of", number, "is", lastnumber, "and is less than 6 and not 0")
+    ln = number % -10
+    if ln > 5:
+        print("Last digit of", number, "is", ln, "and is greater than 5")
+    elif ln < 6 and ln != 0:
+     print("Last digit of", number, "is", ln, "and is less than 6 and not 0")
     else:
-        print("Last digit of", number, "is", lastnumber, "and is 0")
+        print("Last digit of", number, "is", ln, "and is 0")
