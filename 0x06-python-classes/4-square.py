4eab6b3d28a8850c927cba06d219e60dfc33e106
DanielN24 <capdaniel_98@outlook.com>
2022-05-24 20:31:15 -0500

    new file with the answer number 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """initialize a square
         args: size = size of square"""
-        self.__size = size
+        self.size = size
 
     @property   # Getter
     def size(self):
