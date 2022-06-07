e967468e6fca0f13aa1e52e4030478fc73178a18
DanielN24 <capdaniel_98@outlook.com>
2022-06-07 18:25:13 -0500

    correction file 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,12 +6,9 @@ class BaseGeometry:
     """ BaseGeometry class """
 
     def area(self):
-        """ raises an Exception """
-
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ validate if it is a integer """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
