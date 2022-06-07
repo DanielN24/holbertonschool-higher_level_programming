d40202d75a9fe8b5006b82a50247ebab52a81c8b
DanielN24 <capdaniel_98@outlook.com>
2022-06-07 18:39:20 -0500

    correction file 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -18,24 +18,20 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
-    """ class Rectangle that defines a rectangle """
-
+    """
+        Rectangle class
+    """
     def __init__(self, width, height):
-        """ initialize a Rectangle
-        args1: width = width of Rectangle
-        args2: height = height of Rectangle """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
 
-    def area(self):
-        """returns the Rectangle area"""
-
-        return self.__width * self.__height
-
     def __str__(self):
         return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
+
+    def area(self):
+        return (self.__width * self.__height)
 
 
 class Square(Rectangle):
