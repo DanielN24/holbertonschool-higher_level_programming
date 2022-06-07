6c0d8218a4d781e89cb3fee1df41e109d2f2b828
DanielN24 <capdaniel_98@outlook.com>
2022-06-07 18:38:07 -0500

    correction file 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -39,13 +39,14 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
-    """
-        Square class
-    """
+    """ class Square that defines a Square """
+
     def __init__(self, size):
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        return (self.__size ** 2)
+        """returns the Square area"""
+
+        return self.__size * self.__size
