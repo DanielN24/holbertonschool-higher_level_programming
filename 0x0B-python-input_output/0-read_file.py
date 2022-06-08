89f623bfae2d11e64c43e9beb6c758bcbea0bbe2
DanielN24 <capdaniel_98@outlook.com>
2022-06-08 12:44:19 -0500

    correction file 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """ reads a text file """
 
 
-def read_file(filename="my_file_0.txt"):
+def read_file(filename=""):
     """ function that reads a text file and prints it to stdout """
     with open("my_file_0.txt", mode="r", encoding="UTF8") as myFile:
         print(myFile.read())
