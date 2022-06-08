9362bf0eeada6a568617774542db7c139ca57cbb
DanielN24 <capdaniel_98@outlook.com>
2022-06-08 11:14:50 -0500

    ne file with the answer number 0 and file .txt for answer number 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" function that reads a text file """
+""" reads a text file """
 
 
 def read_file(filename="my_file_0.txt"):
+    """ function that reads a text file and prints it to stdout """
     with open("my_file_0.txt", mode="r", encoding="UTF8") as myFile:
         print(myFile.read())
