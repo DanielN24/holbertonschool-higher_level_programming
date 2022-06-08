af2927b1eaf49d817ac67dd5eac436654e9c0736
DanielN24 <capdaniel_98@outlook.com>
2022-06-08 12:41:11 -0500

    correction file 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,4 @@ def write_file(filename="", text=""):
     """ function that writes a string to a text file
     and returns the number of characters written """
     with open("my_first_file.txt", mode="w", encoding="UTF8") as myFile:
-        myFile.write("This School is so cool!\n")
         return(myFile.write(text))
