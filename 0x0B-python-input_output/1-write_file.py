8ad99ed7b50880d8575ca1403074027cbe6910e9
DanielN24 <capdaniel_98@outlook.com>
2022-06-08 11:54:05 -0500

    new file with the answer number 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Write to a file """
+
+
+def write_file(filename="", text=""):
+    """ function that writes a string to a text file
+    and returns the number of characters written """
+    with open("my_first_file.txt", mode="w", encoding="UTF8") as myFile:
+        myFile.write("This School is so cool!\n")
+        myFile = "This School is so cool!\n"
+        return(len(myFile))
