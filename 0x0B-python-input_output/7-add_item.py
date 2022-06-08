3d564ebf4c7e580b445ac4d5dfea040dd9cda8c6
DanielN24 <capdaniel_98@outlook.com>
2022-06-08 17:04:00 -0500

    correction file 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,8 +10,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
     try:
-        validation = load_from_json_file("add_item.json")
+        args = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        validation = []
-    validation.extend(sys.argv[1:])
-    save_to_json_file(validation, "add_item.json")
+        args = []
+    args.extend(sys.argv[1:])
+    save_to_json_file(args, "add_item.json")
