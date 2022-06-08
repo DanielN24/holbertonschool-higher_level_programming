c1d497e80074847d2a6da9aa6bb72502f88d1d2c
DanielN24 <capdaniel_98@outlook.com>
2022-06-08 13:08:42 -0500

    correction filename

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """ function that writes a string to a text file
     and returns the number of characters written """
-    with open("my_first_file.txt", mode="w", encoding="UTF8") as myFile:
+    with open(filename, mode="w", encoding="UTF8") as myFile:
         return(myFile.write(text))
