6a141da01a1fda90effbae8b203d39dd62136274
DanielN24 <capdaniel_98@outlook.com>
2022-06-08 16:59:23 -0500

    correction file 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ function that reads a text file and prints it to stdout """
     with open(filename, encoding="UTF8") as myFile:
-        print(myFile.read())
+        print(myFile.read(), end="")
