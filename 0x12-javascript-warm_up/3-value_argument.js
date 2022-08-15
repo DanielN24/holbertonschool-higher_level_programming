2fa0be60f2890cdf6ec5b30d9db7ec147e971ca9
DanielN24 <capdaniel_98@outlook.com>
2022-08-15 15:14:12 -0500

    correction file 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-const { argv } = require('process');
-
-if (argv.length < 3) {
+const argument = process.argv[2];
+if (argument === undefined) {
   console.log('No argument');
 } else {
-  console.log(argv[2]);
+  console.log(argument);
 }
