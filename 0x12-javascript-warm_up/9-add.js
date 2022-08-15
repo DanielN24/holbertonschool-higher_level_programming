388c5337dd0044aca65eaabb1222a8ff92b4e445
DanielN24 <capdaniel_98@outlook.com>
2022-08-15 14:46:37 -0500

    correction files 9 and 13

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // script that prints the addition of 2 integers
 
-function add(a, b){
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
 const { argv } = require('process');
 
