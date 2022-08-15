6d1110c7cf05502bec0e331909f6a26ab23f377e
DanielN24 <capdaniel_98@outlook.com>
2022-08-15 15:26:03 -0500

    correction file 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const { argv } = require('process');
-
 let secondBig = 0;
-const argument = argv.slice(2);
+const argument = process.argv.slice(2);
 
 if (argument.length > 1) {
   argument.sort();
