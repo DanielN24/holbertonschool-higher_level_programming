fd5972380094fae03c92fc71a723fe7abe4bdc12
DanielN24 <capdaniel_98@outlook.com>
2022-08-15 13:33:05 -0500

    new files with the project

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// script that searches the second biggest integer in the list of arguments.
+
+const { argv } = require("process");
+
+let secondBig = 0;
+const argument = argv.slice(2);
+
+if (argument.length > 1) {
+  argument.sort();
+  secondBig = argument[argument.length - 2];
+}
+console.log(secondBig);
