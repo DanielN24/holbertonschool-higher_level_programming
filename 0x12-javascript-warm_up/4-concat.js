293d03c30555e73093008c73891d68fba76038e4
DanielN24 <capdaniel_98@outlook.com>
2022-08-15 13:50:45 -0500

    new files with the project

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // script that prints two arguments passed to it, in the following format: “ is ”
-import { argv } from 'process';
+const { argv } = require('process');
 if (argv.length){
     console.log(argv[2], ' is ', argv[3]);
 }
