f3178cb29f48dd86b3e15c285433db7b42e59c9a
DanielN24 <capdaniel_98@outlook.com>
2022-08-15 14:33:49 -0500

    correction file 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -10,7 +10,7 @@ if the first argument can be converted to an integer:
 const number = parseInt(argv[2]);
 
 if (isNaN(number)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + number);
+  console.log('My number: ' + number);
 }
