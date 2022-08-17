78841cd3f29597523bcdc3adc79a05cc806c128c
DanielN24 <capdaniel_98@outlook.com>
2022-08-16 20:45:09 -0500

    new files with the project

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,15 +6,16 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-  charPrint(c) {
-      if (c !== undefined){
-          let i = 0;
-          while (i < this.width){
-              i++;
-              console.log(c.repeat(this.height));
-          }
-      } else {
-        this.print()
+
+  charPrint (c) {
+    if (c !== undefined) {
+      let i = 0;
+      while (i < this.width) {
+        i++;
+        console.log(c.repeat(this.height));
+      }
+    } else {
+      this.print();
     }
   }
 }
