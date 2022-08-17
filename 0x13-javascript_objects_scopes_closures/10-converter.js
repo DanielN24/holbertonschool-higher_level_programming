924d45f91aa9d7ca00603383f528e325048dcb63
DanielN24 <capdaniel_98@outlook.com>
2022-08-16 21:16:28 -0500

    new file with the answer number 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,8 +2,5 @@
 // function that converts a number from base 10
 // to another base passed as argument:
 exports.converter = function (base) {
-  function translate (number) {
-    return number.toString(base);
-  }
-  return translate;
+  return num => num.toString(base);
 };
