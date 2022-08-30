f45ce589a8b26f2cba710d5e50982c5dc3d0fbc8
DanielN24 <capdaniel_98@outlook.com>
2022-08-30 15:47:03 -0500

    correction file 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,12 +3,12 @@
 script that lists all states from the database hbtn_0e_0_usa
 '''
 
-import MySQLdb
-from sys import argv
+if __name__ == "__main__":
+    '''module to select states'''
 
+    import MySQLdb
+    from sys import argv
 
-def main():
-    '''main'''
     database = MySQLdb.connect(
         host='localhost', user=argv[1], passwd=argv[2],
         db=argv[3])
@@ -17,7 +17,3 @@ def main():
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-
-
-if __name__ == "__main__":
-    main()
