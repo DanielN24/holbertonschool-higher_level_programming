d4c1478b2a98cc8d52f7274ff507424122f4088e
DanielN24 <capdaniel_98@outlook.com>
2022-08-31 12:18:09 -0500

    new file with the answer number 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,6 +12,5 @@ class State(Base):
     """ State model that links to the table states of 6-model_state.sql.
         """
     __tablename__ = 'states'
-    id = Column(Integer, nullable=False, autoincrement=True,
-                unique=True, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=True)
