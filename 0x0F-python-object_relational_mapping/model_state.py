6e5e6d857e2e1d6b2671cf5fcfdc547533ce4456
DanielN24 <capdaniel_98@outlook.com>
2022-08-31 11:57:13 -0500

    correction file 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,15 +2,16 @@
 """Defines a City model"""
 
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class City(Base):
-    """Represents a city for a MySQL database"""
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+class State(Base):
+    """ State model that links to the table states of 6-model_state.sql.
+        """
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, autoincrement=True,
+                unique=True, primary_key=True)
+    name = Column(String(128), nullable=True)
