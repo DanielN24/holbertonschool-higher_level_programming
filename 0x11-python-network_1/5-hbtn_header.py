d0d2576fc2e13491b3f3b0a3b8ec81a4182e6354
DanielN24 <capdaniel_98@outlook.com>
2022-09-02 10:50:11 -0500

    new file with the answer number 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""displays the value of the variable X-Request-Id """
+
+if __name__ == "__main__":
+    import requests as req
+    import sys
+
+    resp = req.get(sys.argv[1])
+    print(resp.headers['X-Request-Id'])
