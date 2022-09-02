80a680af123554161120dcf0872c91371c44043a
DanielN24 <capdaniel_98@outlook.com>
2022-09-02 11:11:52 -0500

    correction file 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    resp = requests.get(sys.argv[1])
-    print(resp.headers("X-Request-Id"))
+    req = requests.get(sys.argv[1])
+    print(req.headers.get("X-Request-Id"))
