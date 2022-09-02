07ee7c2c7bd49815bea9d0d9804789ac0630c770
DanielN24 <capdaniel_98@outlook.com>
2022-09-02 10:44:04 -0500

    correction file 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status """
 if __name__ == "__main__":
 
     import requests as req
-    
+
     http = req.get("https://intranet.hbtn.io/status")
     http = http.text
     print("Body response:")
