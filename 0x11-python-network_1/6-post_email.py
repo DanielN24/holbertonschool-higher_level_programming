8c7221225ae61766a5f65ff5a9ac71f926b9f961
DanielN24 <capdaniel_98@outlook.com>
2022-09-02 11:15:29 -0500

    correction file 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """sends a POST request to the passed URL with the email """
-
 if __name__ == "__main__":
-    import requests as req
+
+    import requests
     import sys
 
-    email = {'email': sys.argv[2]}
-    r = req.post(sys.argv[1], data=email)
-    print(r.text)
+    email = {"email": sys.argv[2]}
+    req = requests.post(sys.argv[1], data=email)
+    print(req.text)
